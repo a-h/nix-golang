f573c5cf0867892eee7db4be8582c8aeb8425b0b
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-10-19 12:33:55 +0100

    feat: add Go 1.19.1

--- go.nix
+++ go.nix
@@ -7,14 +7,14 @@ let
   hashes = {
     # Use `print-hashes.sh ${version}` to generate the list below
     # https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/compilers/go/print-hashes.sh
-    darwin-amd64 = "df6509885f65f0d7a4eaf3dfbe7dda327569787e8a0a31cbf99ae3a6e23e9ea8";
-    darwin-arm64 = "859e0a54b7fcea89d9dd1ec52aab415ac8f169999e5fdfb0f0c15b577c4ead5e";
-    linux-386 = "6f721fa3e8f823827b875b73579d8ceadd9053ad1db8eaa2393c084865fb4873";
-    linux-amd64 = "464b6b66591f6cf055bc5df90a9750bf5fbc9d038722bb84a9d56a2bea974be6";
-    linux-arm64 = "efa97fac9574fc6ef6c9ff3e3758fb85f1439b046573bf434cccb5e012bd00c8";
-    linux-armv6l = "25197c7d70c6bf2b34d7d7c29a2ff92ba1c393f0fb395218f1147aac2948fb93";
-    linux-ppc64le = "92bf5aa598a01b279d03847c32788a3a7e0a247a029dedb7c759811c2a4241fc";
-    linux-s390x = "58723eb8e3c7b9e8f5e97b2d38ace8fd62d9e5423eaa6cdb7ffe5f881cb11875";
+    darwin-amd64 = "b2828a2b05f0d2169afc74c11ed010775bf7cf0061822b275697b2f470495fb7";
+    darwin-arm64 = "e46aecce83a9289be16ce4ba9b8478a5b89b8aa0230171d5c6adbc0c66640548";
+    linux-386 = "9acc57342400c5b0c2da07b5b01b50da239dd4a7fad41a1fb56af8363ef4133f";
+    linux-amd64 = "acc512fbab4f716a8f97a8b3fbaa9ddd39606a28be6c2515ef7c6c6311acffde";
+    linux-arm64 = "49960821948b9c6b14041430890eccee58c76b52e2dbaafce971c3c38d43df9f";
+    linux-armv6l = "efe93f5671621ee84ce5e262e1e21acbc72acefbaba360f21778abd083d4ad16";
+    linux-ppc64le = "4137984aa353de9c5ec1bd8fb3cd00a0624b75eafa3d4ec13d2f3f48261dba2e";
+    linux-s390x = "ca1005cc80a3dd726536b4c6ea5fef0318939351ff273eff420bd66a377c74eb";
   };
 
   toGoCPU = platform: {
@@ -26,7 +26,7 @@ let
     "powerpc64le" = "ppc64le";
   }.${platform.parsed.cpu.name} or (throw "Unsupported CPU ${platform.parsed.cpu.name}");
 
-  version = "1.19";
+  version = "1.19.1";
 
   toGoPlatform = platform: "${toGoKernel platform}-${toGoCPU platform}";
 
